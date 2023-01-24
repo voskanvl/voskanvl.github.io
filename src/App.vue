f2018e901ca8b7b1ea4954282ec742ff1aa02897
voskanvl <voskanvl@gmail.com>
2023-01-24 16:29:59 +0300

    ErrorMessage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-    <Form>
-        <Field name="email" type="email" v-model="name" />
+    <Form @submit="onSubmit">
+        <Field name="email" type="email" v-model="name" @click="name += '---'" :rules="validate" />
+        <ErrorMessage name="email" />
         <div>{{ name }}</div>
+        <button>Sign up</button>
     </Form>
 </template>
 
 <script setup lang="ts">
-    import { Field, Form } from "vee-validate";
+    import { Field, Form, ErrorMessage } from "vee-validate";
     import { ref } from "vue";
     const count = ref(0);
-    const name = "";
+    const name = ref("");
+    const validate = (value: any) => {
+        console.log(value);
+        if (!value) {
+            return "This field is required";
+        }
+        return true;
+    };
+    function onSubmit(values) {
+        console.log(JSON.stringify(values, null, 2));
+    }
 </script>
 <style scoped lang="sass">
     .count
