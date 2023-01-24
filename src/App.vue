c388f782b26e8998c0dfb3de4f80194efd9c674f
voskanvl <voskanvl@gmail.com>
2023-01-24 16:15:42 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <Form>
+        <Field name="email" type="email" v-model="name" />
+        <div>{{ name }}</div>
+    </Form>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+    import { Field, Form } from "vee-validate";
+    import { ref } from "vue";
+    const count = ref(0);
+    const name = "";
+</script>
+<style scoped lang="sass">
+    .count
+        font-size: 40px
+        user-select: none
 </style>
